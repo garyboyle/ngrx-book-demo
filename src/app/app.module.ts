8513c5874ed795b720a4b49f88e4686e7c5679cc
Gary Boyle <gary@resupplyme.com>
2020-12-29 13:06:36 +0000

    Adding in the book demoe code.  SOme datatype wrangling still needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
+import { StoreModule } from '@ngrx/store';
+import { BookListComponent } from './book-list/books-list.component';
+import { BookCollectionComponent } from './book-collection/book-collection.component';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, BookListComponent, BookCollectionComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
